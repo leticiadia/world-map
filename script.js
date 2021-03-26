e3f7bb97ff215497f2fa87d5d23db5173af04fbd
Leticia Dias <contatoleticiadia@gmail.com>
2021-03-26 10:54:11 -0300

    file

---
+++ script.js
@@ -0,0 +1,9 @@
+var modalBtns = document.querySelectorAll('.modal-open')
+
+modalBtns.forEach(function(btn){
+    btn.onclick = function(){
+        var modal = btn.getAttribute("data-modal")
+
+        document.getElementById(modal).style.display = "block"
+    }
+})
